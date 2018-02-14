cdd3185a8b9f795ab40e082f8be398e7bbf2a54a
remi kajak <l2info041@ic2s122-04.univ-lemans.fr>
2018-02-14 16:31:26 +0100

    Nouveau format du fichier contenant les données des puzzles pour régler les problèmes de lecture de la première fonction d'initilisation des matrices périphériques ; Début du codage de la fonction d'affectation des nombres lus à ces matrices

--- generation.c
+++ generation.c
@@ -2,33 +2,46 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define N 10
+int mat[N][N];
+
+void affectation_nb_matrice(int mat[N][N],int ligne) {
+	
+}
+
 void lecture_fic(char *nom_fic, int niveau) { /* Lecture du fichier contenant les puzzles prédéfinis */
 	FILE * fic_gen;
-	char cle[4] = "PHV-", puzzle, coord;
-	int i, num_puz, nb_case;
+	char cle[4] = "PHVF", carac;
+	int i, num_ligne, num_puz, nb_case;
 	
 	fic_gen = fopen(nom_fic,"r");
-	fscanf(fic_gen,"%c %i",&puzzle,&num_puz);
+	fscanf(fic_gen,"%c %i",&carac,&num_puz);
 	
 	while(!feof(fic_gen)) {
-		if(puzzle == cle[0] && num_puz == niveau) {
-			fscanf(fic_gen,"%c",&coord);
-			if(coord == cle[1]) {
-				while(coord != cle[2]) {
-					fscanf(fic_gen,"%i",&nb_case);
-					/* conditions d'insertion */
-					fscanf(fic_gen,"%c",&coord);
+		if(carac == cle[0] && num_puz == niveau) {
+			printf("%c %i\n",carac,num_puz);
+			for(i = 1; i <= 2; i++) {
+				num_ligne = 0;
+				
+				while(carac != cle[i]) {
+					fscanf(fic_gen,"%c",&carac);
 				}
-			}
-			if(coord == cle[2]) {
-				while(coord != cle[3]) {
-					fscanf(fic_gen,"%i",&nb_case);
-					/* conditions d'insertion */
-					fscanf(fic_gen,"%c",&coord);
+				printf("%c",carac);
+				fscanf(fic_gen,"%c",&carac);
+				while(carac != cle[i+1]) {
+					printf("%c",carac);
+					if(carac == ' ' || carac == '\n') {
+						fscanf(fic_gen,"%i",&nb_case);
+						printf("%i",nb_case);
+						printf("%i\n",num_ligne);
+						if(carac == '\n') num_ligne++;
+					}
+					fscanf(fic_gen,"%c",&carac);
 				}
+				printf("\n\n");
 			}
 		}
-		fscanf(fic_gen,"%c %i",&puzzle,&num_puz);
+		fscanf(fic_gen,"%c %i",&carac,&num_puz);
 	}
 	fclose(fic_gen);
 	printf("Sortie du fichier\n");
